351af6692949c4f93bd38f455f0aa4001bc267b8
Andrés Arreaza <112995166+Andres-Arreaza@users.noreply.github.com>
2024-09-05 17:33:23 +0000

    Correccion del ejercicio 10-Andres Arreaza

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -3,15 +3,13 @@
 // Creamos una funcion la cual elimine la vowels y que las letras se conviertan en mayuscula.
 const rapid = (myString) =>{
     let consonants=[];
-    for(let letter of myString.toUpperCase()){
-        if(['A','E','I','O','U'].includes(letter)==false){
-            consonants.push(letter);
+    for(let letter of myString.toLowerCase()){
+        if(['a','e','i','o','u'].includes(letter)==false){
+            consonants.push(letter.toUpperCase());
         }
     }
-    return consonants.join('');
+    return consonants.join('')
 }
-
-
 // Work above this line; do not change code below
 let str = "John";
 console.log(rapid(str));
